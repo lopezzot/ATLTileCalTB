5faaf381164323cc5119ee16c021297f1529fcec
Stephan Lachnit <stephanlachnit@debian.org>
2022-07-21 16:51:15 +0200

    analysis: add emscale plots
    Signed-off-by: Stephan Lachnit <stephanlachnit@debian.org>

--- analysis/TBrun_all.py
+++ analysis/TBrun_all.py
@@ -103,19 +103,70 @@ def create_r_plot(rdfs: list[ROOT.RDataFrame], name: str) -> ROOT.TCanvas:
     return canvas
 
 
+def create_emscale_plot(rdf_pi: ROOT.RDataFrame, rdf_el: ROOT.RDataFrame, beam_energy: float) -> tuple[ROOT.TH1, ROOT.TCanvas]:
+    ROOT.gStyle.SetOptStat(False)
+    beam_energy_str = str(int(beam_energy)) + ' GeV'
+    canvas = ROOT.TCanvas(f'canvas_emscale_{beam_energy_str}', f'EM-Scale {beam_energy_str} poins', -1280, 720)
+
+    th1 = rdf_el.Histo1D('SdepSum').GetValue()
+    fitres = th1.Fit('gaus', 'SQN').Get()
+    mean = fitres.Parameter(1) / beam_energy
+
+    th1_pi = rdf_pi.Define('SdepSumEMScale', f'SdepSum/{mean}').Histo1D('SdepSumEMScale').GetValue()
+    th1_pi.SetTitle(f'Signal at EM-Scale {beam_energy_str} pions;Reconstructed Energy [GeV];')
+    th1_pi.Draw()
+
+    GLOBAL_ROOT_OBJECT_OWNER.append(th1_pi)
+    GLOBAL_ROOT_OBJECT_OWNER.append(canvas)
+
+    return tuple((th1_pi, canvas))
+
+
+def create_pie_plot(th1s: list[ROOT.TH1]) -> ROOT.TCanvas:
+    ROOT.gStyle.SetOptStat(False)
+    canvas = ROOT.TCanvas(f'canvas_pie', f'pi/e ratio', -1280, 720)
+
+    means = []
+    sigmas = []
+    for count, th1 in enumerate(th1s):
+        fitres = th1.Fit('gaus', 'SQN').Get()
+        means.append(fitres.Parameter(1) / BEAM_ENERGIES[count])
+        sigmas.append(fitres.Parameter(2) / BEAM_ENERGIES[count] / np.sqrt(th1.GetEntries()))
+
+    tge = ROOT.TGraphErrors(len(BEAM_ENERGIES), array('f', BEAM_ENERGIES), array('f', means), 0, array('f', sigmas))
+    tge.SetTitle(f'pi/e ratio;Beam Energy [GeV];R')
+    tge.Draw()
+
+    GLOBAL_ROOT_OBJECT_OWNER.append(tge)
+    GLOBAL_ROOT_OBJECT_OWNER.append(canvas)
+
+    return canvas
+
+
 def analysis(rdfs_el: list[ROOT.RDataFrame], rdfs_pi: list[ROOT.RDataFrame], path: pathlib.Path):
     c_lp_el = create_leakage_plot(rdfs_el, 'electrons', 100.)
     c_lp_el.SaveAs(f'{path}/lp_el.pdf')
     c_lp_pi = create_leakage_plot(rdfs_pi, 'pions', 1000.)
     c_lp_pi.SaveAs(f'{path}/lp_pi.pdf')
+
     c_sp_el = create_signal_plot(rdfs_el, 'electrons')
     c_sp_el.SaveAs(f'{path}/sp_el.pdf')
     c_sp_pi = create_signal_plot(rdfs_pi, 'pions')
     c_sp_pi.SaveAs(f'{path}/sp_pi.pdf')
+
     c_rp_el = create_r_plot(rdfs_el, 'electrons')
     c_rp_el.SaveAs(f'{path}/rp_el.pdf')
     c_rp_pi = create_r_plot(rdfs_pi, 'pions')
     c_rp_pi.SaveAs(f'{path}/rp_pi.pdf')
+
+    emscale_th1s = list[ROOT.TH1]()
+    for count, beam_energy in enumerate(BEAM_ENERGIES):
+        th1_ep, c_ep = create_emscale_plot(rdfs_pi[count], rdfs_el[count], beam_energy)
+        c_ep.SaveAs(f'{path}/ep_{str(int(beam_energy))}.pdf')
+        emscale_th1s.append(th1_ep)
+
+    c_rpiep = create_pie_plot(emscale_th1s)
+    c_rpiep.SaveAs(f'{path}/rpiep.pdf')
 
 
 def parse_args(args: list[str]) -> argparse.Namespace:
