136576ee4632527fff73ad684e25ed3acadc6bba
Stephan Lachnit <stephanlachnit@debian.org>
2022-08-05 11:11:44 +0200

    add more configurations
    Signed-off-by: Stephan Lachnit <stephanlachnit@debian.org>

---
+++ geantval_scripts/configs/10.5.p01.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export G4GCC_VERSION="8"
+export G4GCC_PLATFORM="x86_64-centos7-gcc8-opt"
+export GEANT4_VERSION="10.5.p01"
+export GEANT4_PLATFORM="x86_64-centos7-gcc8-opt-MT"
+export GCC_VERSION="10"
+export GCC_PLATFORM="x86_64-centos7-gcc10-opt"
+
+source /cvmfs/sft.cern.ch/lcg/contrib/gcc/${G4GCC_VERSION}/${G4GCC_PLATFORM}/setup.sh
+source /cvmfs/geant4.cern.ch/geant4/${GEANT4_VERSION}/${GEANT4_PLATFORM}/bin/geant4.sh
+source /cvmfs/sft.cern.ch/lcg/contrib/gcc/${GCC_VERSION}/${GCC_PLATFORM}/setup.sh
+
+# Test path
+GVALPATH="/cvmfs/geant4.cern.ch/opt-geant-val/$GEANT4_VERSION/$GEANT4_PLATFORM/bin:/cvmfs/geant4.cern.ch/opt/$GEANT4_VERSION/$GEANT4_PLATFORM/bin"
+YOURPATH="@YOURPATH@"
+export PATH="$GVALPATH:$PATH:$YOURPATH"
+
+# Geant Val exports
+export VERSION=$GEANT4_VERSION
+export PLATFORM=$GEANT4_PLATFORM
--- scripts/ATLTileCalTB_lxplus_11.0.p02.sh
+++ scripts/ATLTileCalTB_lxplus_11.0.p02.sh
@@ -2,10 +2,10 @@
 
 export ROOT_VERSION="6.24.06"
 export ROOT_PLATFORM="x86_64-centos7-gcc48-opt"
-export G4GCC_VERSION="6.3"
-export G4GCC_PLATFORM="x86_64-centos7-gcc63-opt"
-export GEANT4_VERSION="10.4.p03"
-export GEANT4_PLATFORM="x86_64-centos7-gcc63-opt-MT"
+export G4GCC_VERSION="10"
+export G4GCC_PLATFORM="x86_64-centos7-gcc10-opt"
+export GEANT4_VERSION="11.0.p02"
+export GEANT4_PLATFORM="x86_64-centos7-gcc10-optdeb-MT"
 export GCC_VERSION="10"
 export GCC_PLATFORM="x86_64-centos7-gcc10-opt"
 
