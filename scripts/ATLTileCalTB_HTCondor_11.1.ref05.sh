cd13fa2e341023ce7d3f90f798c501acefe04405
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2023-07-26 18:11:00 +0200

    Add 11.1.ref05 config files
    Add config files for lxplus and HTCondor usage with geant4-11.1.ref05.
    This version will be used for comparison with fluka.cern interface.

---
+++ scripts/ATLTileCalTB_HTCondor_11.1.ref05.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cd /afs/cern.ch/work/l/lopezzot/Fellow/FLUKACERN/ATLTileCalTB-build
+
+export ROOT_VERSION="6.24.06"
+export ROOT_PLATFORM="x86_64-centos7-gcc48-opt"
+export G4GCC_VERSION="10"
+export G4GCC_PLATFORM="x86_64-centos7-gcc10-opt"
+export GEANT4_VERSION="11.1.ref05"
+export GEANT4_PLATFORM="x86_64-centos7-gcc10-optdeb-MT"
+export GCC_VERSION="10"
+export GCC_PLATFORM="x86_64-centos7-gcc10-opt"
+
+source ./ATLTileCalTB_cvmfs_setup.sh
+./ATLTileCalTB -m TBrun_all.mac -t 12 -p FTFP_BERT
