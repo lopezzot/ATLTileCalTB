c2ac79cdb257502859adf10b97edbdf32f2ad6c3
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2024-04-15 21:26:51 +0200

    Fix bug in 11.2.p01 lxplus usage script
    Fix capital letters for GEANT4_PLATFORM.

--- scripts/ATLTileCalTB_lxplus_11.2.p01.sh
+++ scripts/ATLTileCalTB_lxplus_11.2.p01.sh
@@ -5,7 +5,7 @@ export ROOT_PLATFORM="x86_64-almalinux9.3-gcc114-opt"
 export G4GCC_VERSION="12"
 export G4GCC_PLATFORM="x86_64-el9-gcc12-opt"
 export GEANT4_VERSION="11.2.p01"
-export geant4_platform="x86_64-el9-gcc12-optdeb-mt"
+export GEANT4_PLATFORM="x86_64-el9-gcc12-optdeb-MT"
 export GCC_VERSION="12"
 export GCC_PLATFORM="x86_64-el9-gcc12-opt"
 
